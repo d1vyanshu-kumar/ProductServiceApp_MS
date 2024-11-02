88818d206d821af77a24fb7591c486a8a4da8470
divyanshu_Kumar <154233802+d1vyanshu-kumar@users.noreply.github.com>
2024-11-02 16:47:09 +0530

    Implement Circuit Breaker

--- src/main/java/com/BuildMicroservice/product/controller/ProductController.java
+++ src/main/java/com/BuildMicroservice/product/controller/ProductController.java
@@ -34,6 +34,12 @@ public class ProductController {
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<ProductResponse> getAllProducts() {
+       // wait for 5 sec so its simulates the slowly responding Api
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
         return productService.getAllProducts();
     }
 }
